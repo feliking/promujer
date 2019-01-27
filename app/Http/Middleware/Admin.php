287bf004ab25b27f369d9aa1655556f0548ccea8
felix <felixddxd@gmail.com>
2019-01-27 10:25:09 -0400

    configuracion de los middlewares

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Auth;
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()->id != 1){
+            abort(403, "¡No tienes edad para ver este video! le diremos a tus padres.");
+        }
+        return $next($request);
+    }
+}
