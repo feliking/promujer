8a5ee5871f1e3ff8cc0837a5b29be41bf80583ae
felix <felixddxd@gmail.com>
2019-01-27 14:58:57 -0400

    login terminado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,123 @@
-@extends('layouts.app')
+<html lang="en">
+<head>
+	<title>Promujer - Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/vendor/bootstrap/css/bootstrap.min.css")}}>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/fonts/iconic/css/material-design-iconic-font.min.css")}}>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/vendor/animate/animate.css")}}>
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href={{asset("login/vendor/css-hamburgers/hamburgers.min.css")}}>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/vendor/animsition/css/animsition.min.css")}}>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/vendor/select2/select2.min.css")}}>
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href={{asset("login/vendor/daterangepicker/daterangepicker.css")}}>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href={{asset("login/css/util.css")}}>
+	<link rel="stylesheet" type="text/css" href={{asset("login/css/main.css")}}>
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url({{asset('login/images/bg-01.jpg')}});">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+                <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+                    @csrf
+					<span class="login100-form-title p-b-49">
+						SISTEMA DE ADMINISTRACION PROMUJER
+					</span>
+                    
+					<div class="wrap-input100 validate-input m-b-23">
+						<span class="label-input100">Nombre de usuario</span>
+						<input class="input100" type="text" name="username" placeholder="Escriba su nombre de usuario" value={{ old('username')}}>
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+					</div>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="username" value="{{ old('userrname') }}" required autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+					<div class="wrap-input100 validate-input">
+						<span class="label-input100">Contraseña</span>
+						<input class="input100" type="password" name="password" placeholder="Escriba su contraseña">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+					<div class="text-right p-t-8 p-b-31">
+						
+                    </div>
+                    @if ($errors->has('username'))
+                        <div class="alert alert-danger text-center">
+                            <strong>¡Error!</strong> Datos incorrectos, verifique por favor
                         </div>
+                    @endif
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								Ingresar
+							</button>
+						</div>
+					</div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+					{{-- <div class="txt1 text-center p-t-54 p-b-20">
+						<span>
+							Or Sign Up Using
+						</span>
+					</div> --}}
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+					{{-- <div class="flex-c-m">
+						<a href="#" class="login100-social-item bg1">
+							<i class="fa fa-facebook"></i>
+						</a>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+						<a href="#" class="login100-social-item bg2">
+							<i class="fa fa-twitter"></i>
+						</a>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+						<a href="#" class="login100-social-item bg3">
+							<i class="fa fa-google"></i>
+						</a>
+					</div> --}}
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+					{{-- <div class="flex-col-c p-t-155">
+						<span class="txt1 p-b-17">
+							Or Sign Up Using
+						</span>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+						<a href="#" class="txt2">
+							Sign Up
+						</a>
+					</div> --}}
+				</form>
+			</div>
+		</div>
+	</div>
+	
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src={{asset("login/vendor/jquery/jquery-3.2.1.min.js")}}></script>
+<!--===============================================================================================-->
+	<script src={{asset("login/vendor/animsition/js/animsition.min.js")}}></script>
+<!--===============================================================================================-->
+	<script src={{asset("login/vendor/bootstrap/js/popper.js")}}></script>
+	<script src={{asset("login/vendor/bootstrap/js/bootstrap.min.js")}}></script>
+<!--===============================================================================================-->
+	<script src={{asset("login/vendor/select2/select2.min.js")}}></script>
+<!--===============================================================================================-->
+	<script src={{asset("login/vendor/daterangepicker/moment.min.js")}}></script>
+	<script src={{asset("login/vendor/daterangepicker/daterangepicker.js")}}></script>
+<!--===============================================================================================-->
+	<script src={{asset("login/vendor/countdowntime/countdowntime.js")}}></script>
+<!--===============================================================================================-->
+	<script src={{asset("login/js/main.js")}}></script>
+
+</body>
+</html>
